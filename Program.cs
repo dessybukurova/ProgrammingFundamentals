c2be31f7834207675b5a1b0041dd1a1881463253
dessybukurova <32429645+dessybukurova@users.noreply.github.com>
2017-11-15 16:08:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ProgrammingFundamentalsAnonymousDownsite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWebsites = int.Parse(Console.ReadLine());
+
+            int securityKey = int.Parse(Console.ReadLine());
+            decimal totalLoss = 0;
+
+            for (int i = 0; i < numberOfWebsites; i++)
+            {
+                string[] websites = Console.ReadLine().Split();
+                string siteName = websites[0];
+
+                decimal siteVisits = decimal.Parse(websites[1]);
+                decimal siteCommercialPricePerVisit = decimal.Parse(websites[2]);
+                decimal siteLoss = siteVisits * siteCommercialPricePerVisit;
+
+                totalLoss += siteLoss;
+                Console.WriteLine(siteName);
+
+            }
+            Console.WriteLine($"Total loss: {totalLoss:F20}");
+            Console.WriteLine($"Security token: {BigInteger.Pow(securityKey, numberOfWebsites)}");
+        }
+    }
+}
